fa2a5f4e3996190326bf162a03c923244a991ab5
fabri <fabritzioalejandrol@gmail.com>
2025-08-09 20:47:13 -0600

    Metodo para insertar y eliminar un producto

--- actividad17.py
+++ actividad17.py
@@ -4,3 +4,10 @@ class inventario:
     def agregar_producto(self,producto):
         self.productos.append(producto)
         print(f"Producto: {producto} agregado al inventario")
+    def eliminar_producto(self,producto):
+        if producto in self.productos:
+            self.productos.remove(producto)
+            print(f"El producto {producto} ha sido eliminado")
+    def insertar_producto(self,posicion,producto):
+        self.productos.insert(posicion,producto)
+        print(f"Producto {producto} agregado en la posicion {posicion}")
