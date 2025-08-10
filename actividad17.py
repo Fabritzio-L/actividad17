7432763ee7ec44a7a50f2aaa6a8f6fe6123512e4
fabri <fabritzioalejandrol@gmail.com>
2025-08-09 20:52:46 -0600

    Metodo para ordenar, buscar y vaciar inventario

--- actividad17.py
+++ actividad17.py
@@ -8,6 +8,20 @@ class inventario:
         if producto in self.productos:
             self.productos.remove(producto)
             print(f"El producto {producto} ha sido eliminado")
+        else:
+            print("Producto no encontrado")
     def insertar_producto(self,posicion,producto):
         self.productos.insert(posicion,producto)
         print(f"Producto {producto} agregado en la posicion {posicion}")
+    def ordenar_productos(self):
+        self.productos.sort()
+        print("Productos ordenados alfabeticamente")
+    def buscar_producto(self,producto):
+        if producto in self.productos:
+            posicion= self.productos.index(producto)
+            print(f"Producto {producto} esta en la posicion {posicion}")
+        else:
+            print("Producto no encontrado")
+    def vaciar_inventario(self):
+        self.productos.clear()
+        print("Inventario vaciado")
